4cb15039b9c312509c38f8ebb6dc0eeea96a34ee
mgsoelwinoo <39288110+mgsoelwinoo@users.noreply.github.com>
2018-06-26 10:11:22 -0700

    ex101.py

---
+++ ex101.py
@@ -0,0 +1,8 @@
+tabby_cat= "\tI'm tabbed in."
+persian_cat= "I'm split\n on a line."
+backslash_cat="I'm \\ a \\ cat."
+fat_cat= """I'll do a list: \n \t Cat food \n \t Fishies \n \t Catnip \n \t Grass """
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
